702ed149c34a69d7899fff1df199fd7f7067b0a2
kattiwar abhishek <kattiwarabhishek@kattiwars-MacBook-Pro.local>
2025-05-21 11:32:56 +0530

    initial commit

---
+++ mailer/forms_settings.php
@@ -0,0 +1,8 @@
+<?php
+	/* Forms settings */
+	$subj = "New message from the site 'Onovo - Portfolio Agency Template'"; //letter subject
+	$to = 'yourname@domain.com'; // Enter Your E-mail
+	$from = 'admin@domain.com'; // Admin e-mail
+	$fromName = 'Onovo'; // Your company name
+	$charset = 'UTF-8';
+?>
